1460313d429061e6fe16dbff7bf3c828a0a72420
Khimish009 <salman.khimishev01@gmail.com>
2023-10-26 23:49:11 +0300

    feat: add Link to GameTitle

--- components/game/game-title/ui/GameTitle.jsx
+++ components/game/game-title/ui/GameTitle.jsx
@@ -1,3 +1,18 @@
+import Link from "next/link";
+import { ArrayLeftIcon } from "../icons/ArrayLeftIcon";
+
 export function GameTitle() {
-    return 'GameTitle'
+  return (
+    <div>
+      <div className="pl-2">
+        <Link
+          href="#"
+          className="flex items-center gap-2 text-teal-600 text-xs leading-tight -mb-0.5"
+        >
+          <ArrayLeftIcon />
+          На главную
+        </Link>
+      </div>
+    </div>
+  );
 }
